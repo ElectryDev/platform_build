36e190ec566e1665ac32db756cc645d4b416672d
Hamster Tian <haotia@gmail.com>
2016-08-28 17:23:23 +0800

    build: Make override_device actually work
    I'm lost in history...
    
    Change-Id: I85d5784140130966569da1ac4c3330e27d239788

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -117,7 +117,7 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = (' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+    cmd = (' || '.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) + ' || '
            'abort("This package is for \\"%s\\" devices; '
            'this is a \\"" + getprop("ro.product.device") + "\\".");') % (
